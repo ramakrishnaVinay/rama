796796c8f27c56597861b7a34607e50e77e7431a
unknown <dhoni.ramakrishna099@gmail.com>
2018-09-23 03:46:20 -0700

    hai

---
+++ A.java
@@ -0,0 +1,23 @@
+class A 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+class B 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+class C 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
