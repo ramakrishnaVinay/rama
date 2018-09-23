95f5bf05f4838b308a59d3c0be268a86847dd818
unknown <dhoni.ramakrishna099@gmail.com>
2018-09-23 03:55:36 -0700

    Test

---
+++ Test.java
@@ -0,0 +1,13 @@
+import static java.lang.System.out;
+import static java.lang.Integer;
+class Test 
+{
+
+	static String s="java";
+	
+	public static void main(String[] args) 
+	{
+		out.println("Hello World!"+Test.s.length());
+		out.println(MAX_VALUE);
+	}
+}
